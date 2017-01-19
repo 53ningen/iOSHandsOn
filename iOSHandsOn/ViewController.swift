a23376af53cfbcf07bcbd432602587ea01eebca3
gomi_ningen <korun24276@gmail.com>
2017-01-20 03:04:27 +0900

    Storyboard ID を利用したプッシュ遷移を実装

--- iOSHandsOn/ViewController.swift
+++ iOSHandsOn/ViewController.swift
@@ -10,12 +10,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
 
     @IBAction func buttonOnTouchUpInside(_ sender: Any) {
-        let now = Date() // 現在時刻を取得
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ja_JP") // ロケールを指定
-        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss" // 時刻表示のフォーマットを指定
-        // 指定したフォーマットで現在時刻を取得してラベルに反映させる
-        label.text = dateFormatter.string(from: now)
+        let vc = UIStoryboard(name: "Main", bundle: Bundle.main)
+            .instantiateViewController(withIdentifier: "Main") as! ViewController
+        navigationController?.pushViewController(vc, animated: true)
     }
     
 }
