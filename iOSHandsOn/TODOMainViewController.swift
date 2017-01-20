87a76be275e7f8a5bdc1ba25bb75c6d3bd08bd8a
gomi_ningen <korun24276@gmail.com>
2017-01-20 15:35:17 +0900

    テーブルビューの基本実装

--- iOSHandsOn/TODOMainViewController.swift
+++ iOSHandsOn/TODOMainViewController.swift
@@ -9,15 +9,47 @@ import Foundation
 class TODOMainViewController: UIViewController {
     
     @IBOutlet weak var dismissButton: UIBarButtonItem!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dismissButton.target = self
         dismissButton.action = #selector(TODOMainViewController.dismissButtonTapped(_:))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     func dismissButtonTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension TODOMainViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+extension TODOMainViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("selected row at: \(indexPath.item)")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
     }
     
 }
