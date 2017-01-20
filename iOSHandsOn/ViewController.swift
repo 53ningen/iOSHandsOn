ab058fd77b2fe15dd94043676e432f80337cb455
gomi_ningen <korun24276@gmail.com>
2017-01-20 14:48:28 +0900

    モーダル遷移を実装

--- iOSHandsOn/ViewController.swift
+++ iOSHandsOn/ViewController.swift
@@ -10,37 +10,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var pushButton: UIButton!
     @IBOutlet weak var popButton: UIButton!
+    @IBOutlet weak var modalButton: UIButton!
+    @IBOutlet weak var dismissButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pushButton.addTarget(self, action: #selector(ViewController.pushButtonTapped(_:)), for: .touchUpInside)
 
-        // 戻り先の画面が存在するときだけ、ボタンを有効にする
+        // プッシュの戻り先の画面が存在するときだけ、ボタンを有効にする
         popButton.isEnabled = navigationController!.childViewControllers.count > 1
         popButton.addTarget(self, action: #selector(ViewController.popButtonTapped(_:)), for: .touchUpInside)
+        
+        modalButton.addTarget(self, action: #selector(ViewController.modalButtonTapped(_:)), for: .touchUpInside)
+        
+        // モーダルの戻り先が存在するときだけボタンを有効にする
+        dismissButton.isEnabled = presentingViewController != nil
+        
+        // UIBarButtonItem のイベント処理指定は UIButton と少し異なる
+        dismissButton.target = self
+        dismissButton.action = #selector(ViewController.dismissButtonTapped(_:))
     }
     
     // 画面が表示された直後に呼ばれる
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        NSLog("\(navigationController?.childViewControllers)")
+        NSLog("presentingVC: \(presentingViewController)")
+        NSLog("nvc: \(navigationController!.childViewControllers)")
     }
     
     func pushButtonTapped(_ sender: Any) {
-        // Main.storyboard を取得
-        // Main.storyboard の InitialViewController は
-        // UINavigationController なので as! 以後はそれを指定
         let nvc = UIStoryboard(name: "Main", bundle: Bundle.main)
             .instantiateInitialViewController() as! UINavigationController
-        
-        // 遷移先としたいの ViewController は
-        // childViewControllers の 1つ目の要素なので
         let vc = nvc.childViewControllers[0] as! ViewController
         navigationController?.pushViewController(vc, animated: true)
     }
 
     func popButtonTapped(_ sender: Any) {
         _ = navigationController?.popViewController(animated: true)
+    }
+    
+    func modalButtonTapped(_ sender: Any) {
+        // モーダル遷移
+        let nvc = UIStoryboard(name: "Main", bundle: Bundle.main)
+            .instantiateInitialViewController() as! UINavigationController
+        present(nvc, animated: true, completion: nil)
+    }
+    
+    func dismissButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
 }
